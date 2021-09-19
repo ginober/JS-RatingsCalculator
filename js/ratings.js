65d60fd9fbc451c063990d1fefa34d86fbeced45
ginober <reginald.estember@yahoo.com>
2021-09-19 20:31:40 +0800

    Update ratings.js
    added parsing of star values

--- js/ratings.js
+++ js/ratings.js
@@ -1,3 +1,7 @@
 function collect_ratings(){
     const ratings = {'count':0, 'sum':0, 'average':0};
+
+    let rating = 0;
+    const elements = document.querySelectorAll('.rating').forEach(x => parseInt(x.id.replace(/\D/g, "")));
+
 }
