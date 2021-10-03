93ac172de151bb681d09bddd29e64bfb3be6f6fb
ginober <reginald.estember@yahoo.com>
2021-10-03 13:52:55 +0800

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -10,8 +10,8 @@ function collect_ratings(){
         ratings.sum +=parseInt(element.value) * rating;
     });
 
-    if(ratings.count == 0){
-        
+    if(ratings.count !== 0){
+        ratings.average = ratings.sum/ratings.count;
     }
 
 }
